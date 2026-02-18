40e9328e8f46e52244cbd1ba5f7d5e0e8e835032
alex <alejandrocordobarodriguez@gmail.com>
2026-02-18 17:55:23 +0100

    ñdklfgnero

---
+++ src/main/java/com/WhatCanICook/WhatCanICookApplication.java
@@ -0,0 +1,16 @@
+package com.WhatCanICook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatCanICookApplication {
+
+	public static void main(String[] args) {
+
+        System.out.println("HEllo");
+        SpringApplication.run(WhatCanICookApplication.class, args);
+	}
+
+
+}
