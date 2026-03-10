a7eeced55597e1392ae9dccaa82c392228093d64
alex <alejandrocordobarodriguez@gmail.com>
2026-03-10 15:51:58 +0100

    Login

---
+++ app/src/main/java/com/app/ui/login/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.app.ui.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.app.R
+import com.app.databinding.ActivityLoginBinding
+import com.app.databinding.ActivityMainBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
