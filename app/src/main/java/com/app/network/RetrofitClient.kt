fb7b4530da4241dcba194a62b38741db81830086
alex <alejandrocordobarodriguez@gmail.com>
2026-03-11 17:21:00 +0100

    Actualizo Manifest para conectarme a internet y añado android:usesCleartextTraffic="true". Tambien cambio ip en Retrofit

--- app/src/main/java/com/app/network/RetrofitClient.kt
+++ app/src/main/java/com/app/network/RetrofitClient.kt
@@ -1,4 +1,20 @@
 package com.app.network
 
-class RetrofitClient {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private const val BASE_URL = "http://192.168.1.130:8080/"
+
+    /*TODO -> BASE_URL
+    con mv: "http://LA IP DE TU ORDENADOR:8080/"
+    MV -> "http://10.0.2.2:8080/"*/
+
+    val instance: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
