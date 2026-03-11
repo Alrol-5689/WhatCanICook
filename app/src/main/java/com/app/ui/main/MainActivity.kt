deaee6676d6685e6ec0766ca58a9eb7b87c051dc
alex <alejandrocordobarodriguez@gmail.com>
2026-03-11 19:24:08 +0100

    creo MainActivity.kt y activity_main.xmluna tontería para poder volver a login y no ver una pantalla en blanco

--- app/src/main/java/com/app/ui/main/MainActivity.kt
+++ app/src/main/java/com/app/ui/main/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.app.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.app.databinding.ActivityMainBinding
+import com.app.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,6 +16,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun acciones(){
+
+        binding.logoutButton.setOnClickListener {
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }
 
     }
 
