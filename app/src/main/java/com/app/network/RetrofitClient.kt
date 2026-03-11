06712104106b1610884959e74e69a5152251cb83
alex <alejandrocordobarodriguez@gmail.com>
2026-03-11 17:57:45 +0100

    tonteria de commit, me saltaba un fallo en un comentario. Ingorar

--- app/src/main/java/com/app/network/RetrofitClient.kt
+++ app/src/main/java/com/app/network/RetrofitClient.kt
@@ -8,7 +8,7 @@ object RetrofitClient {
     private const val BASE_URL = "http://192.168.1.130:8080/"
 
     /* -> BASE_URL =
-    con mv: "http://LA IP DE TU ORDENADOR:8080/"   || my y PC en la misma wifi ||
+    con mv: "http://LA IP DE TU ORDENADOR:8080/"   || my y PC en la misma Wi-Fi ||
     MV -> "http://10.0.2.2:8080/"*/
 
     val instance: Retrofit by lazy {
