59e226c20507c2d5c8ee28684a82d562747aa024
alex <alejandrocordobarodriguez@gmail.com>
2026-03-11 17:33:25 +0100

    Modifico comentario en RetrofitClient.kt

--- app/src/main/java/com/app/network/RetrofitClient.kt
+++ app/src/main/java/com/app/network/RetrofitClient.kt
@@ -7,8 +7,8 @@ object RetrofitClient {
 
     private const val BASE_URL = "http://192.168.1.130:8080/"
 
-    /*TODO -> BASE_URL
-    con mv: "http://LA IP DE TU ORDENADOR:8080/"
+    /* -> BASE_URL =
+    con mv: "http://LA IP DE TU ORDENADOR:8080/"   || my y PC en la misma wifi ||
     MV -> "http://10.0.2.2:8080/"*/
 
     val instance: Retrofit by lazy {
