0008215879041fb9959b72cba02db74902a0a14a
alex <alejandrocordobarodriguez@gmail.com>
2026-03-12 01:59:59 +0100

    pongo el id de las Activitties con el ide correcto

--- app/src/main/java/com/app/ui/recipes/RecipeDetailActivity.kt
+++ app/src/main/java/com/app/ui/recipes/RecipeDetailActivity.kt
@@ -12,7 +12,7 @@ class RecipeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_recipe_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.recipes_details)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
