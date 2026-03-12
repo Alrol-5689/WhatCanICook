f41391f087542a788d0dfc91478dbb9d99f7e9bf
alex <alejandrocordobarodriguez@gmail.com>
2026-03-12 01:24:00 +0100

    MainActivity.kt ahora recibe nombre y email

--- app/src/main/java/com/app/ui/main/MainActivity.kt
+++ app/src/main/java/com/app/ui/main/MainActivity.kt
@@ -8,14 +8,22 @@ import com.app.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var username: String? = null
+    private var email: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        username = intent.getStringExtra("username")
+        email = intent.getStringExtra("email")
         acciones()
     }
 
     private fun acciones(){
+
+        binding.textUsername.text = "Eres: ${username ?: "desconocido"}"
+        binding.textEmail.text = "Tu correo es: ${email ?: "desconocido"}"
 
         binding.logoutButton.setOnClickListener {
 
