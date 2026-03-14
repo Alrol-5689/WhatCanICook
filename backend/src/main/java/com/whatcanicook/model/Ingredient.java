5ff28385df43b0baa387bc95fd8aed6301dbbed6
alex <alejandrocordobarodriguez@gmail.com>
2026-03-14 12:33:48 +0100

    Ingredient.java -> pongo etiquetas JsonPropery porque solo cargaba el nombre del alimento en DB

--- backend/src/main/java/com/whatcanicook/model/Ingredient.java
+++ backend/src/main/java/com/whatcanicook/model/Ingredient.java
@@ -1,5 +1,6 @@
 package com.whatcanicook.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -20,15 +21,19 @@ public class Ingredient {
     @Column(nullable = false, unique = true, length = 100)
     private String name;
 
+    @JsonProperty("carbs_100g")
     @Column(name = "carbs_100g")
     private Double carbs100g;
 
+    @JsonProperty("protein_100g")
     @Column(name = "protein_100g")
     private Double protein100g;
 
+    @JsonProperty("fat_100g")
     @Column(name = "fat_100g")
     private Double fat100g;
 
+    @JsonProperty("fiber_100g")
     @Column(name = "fiber_100g")
     private Double fiber100g;
 }
