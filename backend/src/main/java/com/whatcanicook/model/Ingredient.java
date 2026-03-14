4f113389bacb6936f19a865cec909ee899b64d94
alex <alejandrocordobarodriguez@gmail.com>
2026-03-14 12:03:02 +0100

    Ingredient.java || añado propiedades ajustadas al json

--- backend/src/main/java/com/whatcanicook/model/Ingredient.java
+++ backend/src/main/java/com/whatcanicook/model/Ingredient.java
@@ -19,4 +19,16 @@ public class Ingredient {
 
     @Column(nullable = false, unique = true, length = 100)
     private String name;
+
+    @Column(name = "carbs_100g")
+    private Double carbs100g;
+
+    @Column(name = "protein_100g")
+    private Double protein100g;
+
+    @Column(name = "fat_100g")
+    private Double fat100g;
+
+    @Column(name = "fiber_100g")
+    private Double fiber100g;
 }
