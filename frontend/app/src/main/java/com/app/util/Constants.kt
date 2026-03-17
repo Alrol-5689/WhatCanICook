bfab4c3b0b746613531dc747628902b3290eec2a
alex <alejandrocordobarodriguez@gmail.com>
2026-03-17 19:49:05 +0100

    añado comentarios para futuros usos de Constants y SessionManager.kt

--- frontend/app/src/main/java/com/app/util/Constants.kt
+++ frontend/app/src/main/java/com/app/util/Constants.kt
@@ -1,4 +1,9 @@
 package com.app.util
 
 class Constants {
+
+    /*Solo si ya vamos a meter constantes reales. Por ejemplo:
+    •	claves de Intent
+    •	claves de SharedPreferences
+    •	quizá nombres de endpoints si los reutilizas mucho*/
 }
--- frontend/app/src/main/java/com/app/util/SessionManager.kt
+++ frontend/app/src/main/java/com/app/util/SessionManager.kt
@@ -1,4 +1,7 @@
 package com.app.util
 
 class SessionManager {
+
+    // Ahora mismo en LoginActivity pasamos datos por Intent a MainActivity.
+    // Funciona, pero SessionManager nos vendrá mejor para persistencia real.
 }
